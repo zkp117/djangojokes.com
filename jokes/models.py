6b488e6fe406858fd688d3313adf9d2bcd006758
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-13 19:12:15 -0500

    updating jokes

--- jokes/models.py
+++ jokes/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('jokes:detail', args=[str(self.pk)])
 
     def __str__(self):
         return self.question
