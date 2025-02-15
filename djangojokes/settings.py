4e84e043e76efa552752f14b58034db45bb30895
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-15 12:33:26 -0500

    update table

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'djangojokes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jokes',
+        'USER': 'postgres',
+        'PASSWORD': 'Pandora117!',
+        'HOST': 'localhost',
+        'PORT': 0622
     }
 }
 
