5a0c528c2fef55e9da3d3ba50f40d8c278237e3a
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-15 14:44:28 -0500

    updating

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-     path('jokes/', include('jokes.urls')),
+    path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
 ]
