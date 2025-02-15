eae903d2b558a17633bbec0a53421f698314c848
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-15 14:53:44 -0500

    lal

--- jokes/urls.py
+++ jokes/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
 from .views import (
-    JokeCreateView, JokeDeleteView, JokeDetailView, JokeListView, JokeUpdateView
+    JokeCreateView, JokeDeleteView, JokeDetailView, JokeListView,
+    JokeUpdateView
 )
 
 app_name = 'jokes'
 urlpatterns = [
-    path('joke/<int:pk>/update/', JokeUpdateView.as_view(), name='update'),
-    path('joke/<int:pk>/delete/', JokeDeleteView.as_view(), name='delete'),
+    path('joke/<slug>/update/', JokeUpdateView.as_view(), name='update'),
+    path('joke/<slug>/delete/', JokeDeleteView.as_view(), name='delete'),
     path('joke/create/', JokeCreateView.as_view(), name='create'),
-    path('joke/<int:pk>/', JokeDetailView.as_view(), name='detail'),
+    path('joke/<slug>/', JokeDetailView.as_view(), name='detail'),
     path('', JokeListView.as_view(), name='list'),
 ]
