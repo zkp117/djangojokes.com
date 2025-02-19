a327b1b973667b3700717ad8ec2c37f851f77b26
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-18 20:33:00 -0500

    yezz

--- djangojokes/send_email.py
+++ djangojokes/send_email.py
@@ -1,19 +1,19 @@
-# using SendGrid's Python Library
-# https://github.com/sendgrid/sendgrid-python
-import os
-from sendgrid import SendGridAPIClient
+import ssl
+import certifi
+import sendgrid
 from sendgrid.helpers.mail import Mail
 
+sg = sendgrid.SendGridAPIClient(api_key="YOUR_API_KEY")
 message = Mail(
-    from_email='pandoraparigian@gmail.com',
-    to_emails='pandoraparigian@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
-try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.message)
+    from_email="pandoraparigian@gmail.com",
+    to_emails="pandoraparigian@gmail.com",
+    subject="Test Email",
+    plain_text_content="Hello from DjangoJokes!"
+)
+
+# Create an SSL context that uses the correct certificates
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+# Send email with SSL context
+response = sg.client.mail.send.post(request_body=message.get(), ssl_context=ssl_context)
+print(response.status_code)
