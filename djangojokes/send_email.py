30f6c7f284c05c8bf7f4dadc4ea308602ca26879
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-19 19:19:33 -0500

    yezz

--- djangojokes/send_email.py
+++ djangojokes/send_email.py
@@ -1,15 +1,18 @@
 import sendgrid
-import os
-from sendgrid.helpers.mail import *
+from sendgrid.helpers.mail import Mail
 
 sg = sendgrid.SendGridAPIClient(api_key="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")
-from_email = Email("pandoraparigian@gmail.com")
-to_email = To("pandoraparigian@gmail.com")
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, to_email, subject, content)
-response = sg.client.mail.send.post(request_body=mail.get())
-print(response.status_code)
-print(response.body)
-print(response.headers)
-print(mail.get())
+message = Mail(
+    from_email="pandoraparigian@gmail.com",
+    to_emails="pandoraparigian@gmail.com",
+    subject="Test Email",
+    plain_text_content="This is a test email from SendGrid."
+)
+
+try:
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e)
