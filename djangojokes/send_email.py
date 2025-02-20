008d18938c781dadd03fe3679cb0e07d181d57d9
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-19 19:16:34 -0500

    yezz

--- djangojokes/send_email.py
+++ djangojokes/send_email.py
@@ -2,7 +2,7 @@ import sendgrid
 import os
 from sendgrid.helpers.mail import *
 
-sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+sg = sendgrid.SendGridAPIClient(api_key="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")
 from_email = Email("pandoraparigian@gmail.com")
 to_email = To("pandoraparigian@gmail.com")
 subject = "Sending with SendGrid is Fun"
@@ -12,3 +12,4 @@ response = sg.client.mail.send.post(request_body=mail.get())
 print(response.status_code)
 print(response.body)
 print(response.headers)
+print(mail.get())
