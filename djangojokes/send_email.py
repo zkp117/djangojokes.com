effa6f612273b7366489ccf5737fe3c64a648327
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-19 19:13:02 -0500

    yezz

--- djangojokes/send_email.py
+++ djangojokes/send_email.py
@@ -1,19 +1,14 @@
-# using SendGrid's Python Library
-# https://github.com/sendgrid/sendgrid-python
+import sendgrid
 import os
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import *
 
-message = Mail(
-    from_email='pandoraparigian@gmail.com',
-    to_emails='pandoraparigian@gmail.com',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
-try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.message)
+sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+from_email = Email("pandoraparigian@gmail.com")
+to_email = To("pandoraparigian@gmail.com")
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+mail = Mail(from_email, to_email, subject, content)
+response = sg.client.mail.send.post(request_body=mail.get())
+print(response.status_code)
+print(response.body)
+print(response.headers)
