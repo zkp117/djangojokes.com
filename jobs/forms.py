8231e3aab5e3f76f47626457bc32f99d13114576
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-22 12:17:04 -0500

    ywzz

---
+++ jobs/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class JobApplicationForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView, TemplateView
 
-# Create your views here.
+from .forms import JobApplicationForm
+
+class JobAppView(FormView):
+    template_name = 'jobs/joke_writer.html'
+    form_class = JobApplicationForm
+    success_url = reverse_lazy('jobs:thanks')
+
+class JobAppThanksView(TemplateView):
+    template_name = 'jobs/thanks.html'
