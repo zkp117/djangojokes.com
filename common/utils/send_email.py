c155dbc9278cd76786754561c5c156b5c44e27d1
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-22 13:17:03 -0500

    yezz

--- common/utils/send_email.py
+++ common/utils/send_email.py
@@ -5,14 +5,19 @@ import os
 def send_email(to, subject, content):
     SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
 
+    if not SENDGRID_API_KEY:
+        raise ValueError("SendGrid API Key is missing! Check your environment variables.")
+
+    print("Using SendGrid API Key:", SENDGRID_API_KEY[:5] + "*****")
+
     sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
     
     email = Mail(
-        from_email="pandoraparigian@gmail.com",
-        to_emails=to,
+        from_email="pandoraparigian@gmail.com", 
         subject=subject,
         html_content=content
     )
-    
+
     response = sg.send(email)
+    print("SendGrid Response:", response.status_code)
     return response.status_code
