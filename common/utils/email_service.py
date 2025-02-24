068e8be34698cdb9eb503bc68112cb02442d0056
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-02-24 14:51:58 -0500

    yezz

--- common/utils/email_service.py
+++ common/utils/email_service.py
@@ -2,11 +2,11 @@ import sendgrid
 from sendgrid.helpers.mail import Mail
 import os
 
-def send_email(to, subject, content):
+def send_email(to_email, subject, content):
     sg = sendgrid.SendGridAPIClient(api_key=os.getenv('SENDGRID_API_KEY')) 
     email = Mail(
         from_email='pandoraparigian@gmail.com',
-        to_emails=to,
+        to_emails=to_email,
         subject=subject,
         plain_text_content=content
     )
