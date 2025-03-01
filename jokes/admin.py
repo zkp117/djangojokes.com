e76498df3a8729e6129bafd3a557f7223bace180
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-01 16:08:15 -0500

    yezz

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from .models import Category, Joke
 
-from .models import Joke
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['category', 'created', 'updated']
 
+    def get_readonly_fields(self, request, obj = None):
+        if obj: # editing an existing object
+            return ('slug', 'created', 'updated')
+        return()
 @admin.register(Joke)
 class JokeAdmin(admin.ModelAdmin):
     model = Joke
