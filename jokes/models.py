877a6620f9a3e9cdce40590d2ec137ecd7320360
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-01 15:53:37 -0500

    yezz

--- jokes/models.py
+++ jokes/models.py
@@ -5,7 +5,7 @@ class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
     category = models.ForeignKey(
-    'Category', on_delete=models.PROTECT, null=True
+        'Category', on_delete=models.PROTECT, null=True
     )
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
@@ -18,28 +18,26 @@ class Joke(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug(self.question[:50], type(self))
-            super().save(*args, **kwargs)
+            self.slug = unique_slug(self.question[:50], type(self))  # Fixed line
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.question
-    
 class Category(models.Model):
     category = models.CharField(max_length=50)
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
     )
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now= True)
+    updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse('jokes:category', args=[self.slug])
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
-            value = str(self)
-            self.slug = unique_slug(value, type(self))
+            self.slug = unique_slug(self.category[:50], type(self))  # Fixed line
         super().save(*args, **kwargs)
 
-        def __str__(self):
-            return self.category
+    def __str__(self):  # Moved outside of `save()`
+        return self.category
