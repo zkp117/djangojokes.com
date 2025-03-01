4367e7aea52e7c6bebd2397ca93588cee9617f51
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-01 15:50:08 -0500

    yez

--- jokes/models.py
+++ jokes/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-
 from common.utils.text import unique_slug
 class Joke(models.Model):
     question = models.TextField(max_length=200)
@@ -19,10 +18,8 @@ class Joke(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            value = str(self)
-            self.slug = unique_slug(value, type(self))
-
-        super().save(*args, **kwargs)
+            self.slug(self.question[:50], type(self))
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return self.question
