9ed3adedca1151c75e9d89c3041779eb9664b1ae
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-01 15:40:08 -0500

    yezz

--- jokes/models.py
+++ jokes/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 
 from common.utils.text import unique_slug
 
+category = models.ForeignKey('Category', on_delete=models.PROTECT)
 class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
@@ -24,3 +25,23 @@ class Joke(models.Model):
 
     def __str__(self):
         return self.question
+    
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now= True)
+
+    def get_absolute_url(self):
+        return reverse('jokes:category', args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+        def __str__(self):
+            return self.category
