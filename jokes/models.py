743c9a9906e714402bf81965dbd9c1cc53012bf2
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-02 13:23:46 -0500

    yez

--- jokes/models.py
+++ jokes/models.py
@@ -8,6 +8,8 @@ class Joke(models.Model):
     category = models.ForeignKey(
         'Category', on_delete=models.PROTECT)
     
+    tags = models.ManyToManyField('Tag')
+ 
     slug = models.SlugField(
     max_length=50, unique=True, null=False, editable=False, default=''
 )
@@ -48,3 +50,23 @@ class Category(models.Model):
     
     class Meta:
         verbose_name_plural = 'Categories'
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('jokes:tag', args=[self.slug])
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+            super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.tag
