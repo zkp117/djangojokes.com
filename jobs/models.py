2f68051ecfb6f61bad5a2c0c4d1544a418c9d85d
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-05 19:58:42 -0500

    yezz

--- jobs/models.py
+++ jobs/models.py
@@ -40,7 +40,7 @@ class Applicant(models.Model):
     )
     available_days = models.CharField(max_length=20, default="Please choose")
     desired_hourly_wage = models.DecimalField(max_digits=5, decimal_places=2)
-    cover_letter = models.TextField()
+    cover_letter = models.TextField(default="No cover letter provided")
     confirmation = models.BooleanField(default=False)
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
