0f807e5587dc262b6440b03f4e1feb542d82c1d9
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-07 11:49:20 -0500

    yez

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.gitlab'
 
     # Local apps
     'common.apps.CommonConfig',
