33ee8fe6619a2d5dcb14afc7081e77e3f07252c4
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-07 11:33:36 -0500

    yez

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Admin
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
+
+    # Local Apps
     path('jobs/', include('jobs.urls')),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
+
+    # User Management
+    path('account/', include('allauth.urls'))
 ]
