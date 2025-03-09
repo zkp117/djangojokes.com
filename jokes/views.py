121fe6a63c10de2c68a4df6c0fb5018aa72c6dac
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-09 12:15:00 -0400

    yezz

--- jokes/views.py
+++ jokes/views.py
@@ -1,13 +1,12 @@
-from django.urls import reverse_lazy
+from .models import Joke
+from .forms import JokeForm
 
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
-
-from .models import Joke
-from .forms import JokeForm
-
-class JokeCreateView(CreateView):
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+class JokeCreateView(LoginRequiredMixin, CreateView):
     model = Joke
     form_class = JokeForm
 
