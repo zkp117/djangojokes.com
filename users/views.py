4931a9d9de4d93b19a925780e1f9415d20b88250
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-09 11:52:03 -0400

    ywz

--- users/views.py
+++ users/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 
+from allauth.account.views import PasswordChangeView
+
 from .forms import CustomUserChangeForm
+
+class CustomPasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('my-account')
 
 class MyAccountPageView(UpdateView):
     model = get_user_model()
