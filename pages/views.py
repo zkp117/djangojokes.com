b5b1f4d04e74c8e2cac6da2baebe890e586d7c7d
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-09 13:26:19 -0400

    yezz

--- pages/views.py
+++ pages/views.py
@@ -6,11 +6,11 @@ class AboutUsView(TemplateView):
     
     template_name = 'pages/about_us.html'
 
-    def get(self, request, *args, **kwargs):
-        messages.debug(request, 'Debug message.')
-        messages.info(request, 'Info message.')
-        messages.success(request, 'Success message.')
-        messages.warning(request, 'Warning message.')
-        messages.error(request, 'Error message.')
-        return super().get(request, args, kwargs)
+def get(self, request, *args, **kwargs):
+    messages.debug(request, 'Debug message.')
+    messages.info(request, 'Info message.')
+    messages.success(request, 'Success message.')
+    messages.warning(request, 'Warning message.')
+    messages.error(request, 'Error message.')
+    return super().get(request, args, kwargs)
     
