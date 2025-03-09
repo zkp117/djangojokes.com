1cadad6cad56cf63edb422247b49ba2c755934c4
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-09 13:25:15 -0400

    yezz

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,16 @@
 from django.views.generic import TemplateView
-
+from django.contrib import messages
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
-
-
 class AboutUsView(TemplateView):
+    
     template_name = 'pages/about_us.html'
+
+    def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Debug message.')
+        messages.info(request, 'Info message.')
+        messages.success(request, 'Success message.')
+        messages.warning(request, 'Warning message.')
+        messages.error(request, 'Error message.')
+        return super().get(request, args, kwargs)
+    
