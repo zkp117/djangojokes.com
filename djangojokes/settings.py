4dbc8736e2d25c753a04cb3cd9baca11450d60d0
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-10 14:19:17 -0400

    ere

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -202,14 +202,12 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_S3_REGION_NAME = 'us-east-2'  
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'djangojokes.storage_backends.StaticStorage'
+DEFAULT_FILE_STORAGE = 'djangojokes.storage_backends.PublicMediaStorage'
 PRIVATE_FILE_STORAGE = 'djangojokes.storage_backends.PrivateMediaStorage'
 
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_dummy')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
 
