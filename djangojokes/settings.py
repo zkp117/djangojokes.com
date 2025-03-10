7ac015411436a5a9a7baa6436bfa63cf0a02ad6b
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-10 15:08:26 -0400

    weqwe

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -192,10 +192,10 @@ USE_TZ = True
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = '031025-django-bucket'
+AWS_STORAGE_BUCKET_NAME = 'django-jokes2025'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
