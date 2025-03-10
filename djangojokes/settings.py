0b37a37642c12975e9df9579c555de61957ed50a
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-10 17:03:29 -0400

    wwewqe

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -202,7 +202,15 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_S3_REGION_NAME = 'us-east-2'  
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STORAGES = {
+   "default": {
+        "BACKEND" : "storages.backends.s3boto3.S3StaticStorage",
+    },
+
+    "staticfiles":  {
+        "BACKEND" : "storages.backends.s3boto3.S3StaticStorage",
+    },
+}
 DEFAULT_FILE_STORAGE = 'djangojokes.storage_backends.PublicMediaStorage'
 PRIVATE_FILE_STORAGE = 'djangojokes.storage_backends.PrivateMediaStorage'
 
