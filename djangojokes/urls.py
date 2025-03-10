d3bcd9a9b20af9631efd9dee30bb0e37192c2d23
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-10 10:57:01 -0400

    yezz

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     # Admin
     path('admin/doc/', include('django.contrib.admindocs.urls')),
@@ -14,4 +17,4 @@ urlpatterns = [
     path('jobs/', include('jobs.urls')),
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
