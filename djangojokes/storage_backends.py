b752d24ad69ef79b9f08da17794329a8bfab7cb6
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-12 13:22:48 -0400

    asasa

--- djangojokes/storage_backends.py
+++ djangojokes/storage_backends.py
@@ -6,10 +6,9 @@ class StaticStorage(S3Boto3Storage):
     file_overwrite = False
 
 class PublicMediaStorage(S3Boto3Storage):
-    """ Class for storing public media files. """
-    location = 'media'
-    default_acl = 'public-read'
-    file_overwrite = False
+    location = "media"  # Ensures files go to "media/" instead of "static/"
+    default_acl = "public-read"  # Makes avatars publicly accessible
+    file_overwrite = False  # Prevents overwriting existing files
 
 class PrivateMediaStorage(S3Boto3Storage):
     """ Class for storing private media files. """
