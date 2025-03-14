5a4be028cf4c591513217e02d9d271bd9eba9f7a
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-14 16:27:35 -0400

    212121212

--- jokes/models.py
+++ jokes/models.py
@@ -108,9 +108,22 @@ class JokeVote(models.Model):
     class MyModel(models.Model):
         field_name = models.TextField()
         
-        class Meta:
-            constraints = [
-                models.UniqueConstraint(
-                    fields=['user', 'joke'], name='one_vote_per_user_per_joke'
+class JokeVote(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    joke = models.ForeignKey(
+        Joke, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    vote = models.SmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'joke'], name='one_vote_per_user_per_joke'
             )
-        ]
+        ]       
