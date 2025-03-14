7b395771baa5305e7a840e30419bafbd6b3e11ed
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-14 16:55:55 -0400

    qwqw

--- static/js/jokes.js
+++ static/js/jokes.js
@@ -8,6 +8,13 @@ window.addEventListener('load', () => {
   })
   
   function register(vote) {
+
+    if (!isAuthenticated) {
+        const outputDiv = document.getElementById('output');
+        outputDiv.innerHTML = 'Sorry, only logged-in users can vote.';
+        return false;
+      }
+
     const csrfInput =  document.querySelector("input[name='csrfmiddlewaretoken']");
     const csrfToken = csrfInput.value;
     const likes = Number(document.getElementById('likes').innerHTML);
