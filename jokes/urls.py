ab13802a2c455379ebaed7239dddfd59ae9941a6
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-14 15:43:49 -0400

    qqwqwq

--- jokes/urls.py
+++ jokes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
     JokeCreateView, JokeDeleteView, JokeDetailView, JokeListView,
-    JokeUpdateView
+    JokeUpdateView, vote
 )
 
 app_name = 'jokes'
@@ -11,5 +11,6 @@ urlpatterns = [
     path('joke/<slug>/delete/', JokeDeleteView.as_view(), name='delete'),
     path('joke/create/', JokeCreateView.as_view(), name='create'),
     path('joke/<slug>/', JokeDetailView.as_view(), name='detail'),
+    path('joke/<slug>/vote/', vote, name='ajax-vote'),
     path('', JokeListView.as_view(), name='list'),
 ]
