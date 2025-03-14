2f0b66812e3938108cac5d1965047acc79ccf812
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-14 11:11:31 -0400

    343432

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -196,13 +196,12 @@ AWS_S3_OBJECT_PARAMETERS = { 'CacheControl': 'max-age=86400', }
 AWS_S3_REGION_NAME = 'us-east-2'  
 
 STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",  # Change this line
-    },
     "staticfiles": {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
     },
 }
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_LOCATION = 'static'
