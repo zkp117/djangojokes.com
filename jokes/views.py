ffdc5669bd000381574595ae696f652f77120c70
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-15 12:56:48 -0400

    qwqwqw

--- jokes/views.py
+++ jokes/views.py
@@ -3,6 +3,7 @@ from .models import Joke, JokeVote
 from .forms import JokeForm
 from django.contrib import messages
 from django.http import JsonResponse
+from django.db.models import Q
 
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
@@ -78,6 +79,12 @@ class JokeListView(ListView):
     def get_queryset(self):
         ordering = self.get_ordering()
         qs = Joke.objects.all()
+
+        if 'q' in self.request.GET: # Filter by search query
+            q = self.request.GET.get('q') 
+            qs = qs.filter(
+                Q(question__icontains=q) | Q(answer__icontains=q)
+                )
         
         if 'slug' in self.kwargs: # Filter by category or tag
             slug = self.kwargs['slug']
