a9d0a88f9cbb6039016beaed29e3e59140cc20ab
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-16 13:00:24 -0400

    wqwqwq

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -75,7 +75,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 if DEBUG:
