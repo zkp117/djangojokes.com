b9af8552215b9cceab4717b6f7bf74156bb3a74b
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-16 17:30:42 -0400

    qqwqwq

--- users/admin.py
+++ users/admin.py
@@ -4,6 +4,8 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.safestring import mark_safe
 from django.urls import reverse
 
+from allauth.socialaccount.models import SocialApp, SocialAccount, SocialToken
+
 from common.admin import DjangoJokesAdmin
 from common.utils.admin import append_fields, move_fields, remove_fields
 
@@ -13,32 +15,41 @@ CustomUser = get_user_model()
 class CustomUserAdmin(DjangoJokesAdmin, UserAdmin):
     model = CustomUser
 
+    readonly_fields = ['password_form']
+
     # List Attributes
     list_display = UserAdmin.list_display + ('is_superuser',)
     list_display_links = ('username', 'email', 'first_name', 'last_name')
 
     # Fields for editing existing user.
     new_fields = ('dob', 'avatar')
-
+    # Add new fields to 'Personal info' section.
     append_fields(UserAdmin.fieldsets, 'Personal info', new_fields)
-    append_fields(UserAdmin.fieldsets, None, ('password_form',))
+    # Move email field from 'Personal info' section to unlabelled section
     move_fields(UserAdmin.fieldsets, 'Personal info', None, ('email',))
+    # Remove password field from 'Personal info'
     remove_fields(UserAdmin.fieldsets, None, ('password',))
+    append_fields(UserAdmin.fieldsets, None, ('password_form',))
 
-    readonly_fields = ['password_form']
-
+    # Fields for adding new user.
     new_fields = ('email', )
+    # Add new fields to unlabelled section.
     add_fieldsets = append_fields(UserAdmin.add_fieldsets, None, new_fields)
 
     # Add optional fields to new 'Optional Fields' section.
     optional_fields = ('first_name', 'last_name', 'dob')
     add_fieldsets = append_fields(UserAdmin.add_fieldsets, 'Optional Fields', optional_fields)
 
+    def password_form(self, obj):
+        url = reverse('admin:auth_user_password_change', args=[obj.pk])
+        return mark_safe(f'<a href="{url}">Change Password</a>')
+
     # Add Save buttons to the top of the change user form
     def get_form(self, request, obj=None, **kwargs):
         self.save_on_top = obj is not None
         return super().get_form(request, obj, **kwargs)
-    
-    def password_form(self, obj):
-        url = reverse('admin:auth_user_password_change', args=[obj.pk])
-        return mark_safe(f'<a href="{url}">Change Password</a>')
+
+
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialToken)
