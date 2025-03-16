a4b5b0afaabf089a7ef1f487e5c4af66cd433fe9
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-16 12:32:53 -0400

    aasasa

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -21,9 +21,9 @@ SECRET_KEY = 'django-insecure-^%ns@$smw%itx!1043hfh^cz6zk^18x0%w(2&=ufs#38^8d%c7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djangojoke-0822cf42e9b9.herokuapp.com']
+ALLOWED_HOSTS = ['http://www.django-jokes.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://djangojoke-0822cf42e9b9.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['http://www.django-jokes.com']
 
 INTERNAL_IPS = [
     os.getenv("DJANGO_INTERNAL_IP", "127.0.0.1")  # Default to localhost
