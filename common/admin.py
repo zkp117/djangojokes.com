790770e0bf5a7b57c1e827f9200eef681fc341cb
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-03-16 16:36:54 -0400

    sasa

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.index_title = 'Home'
+admin.site.site_title = 'Django Jokes Admin'
+admin.site.site_header = 'Django Jokes Admin'
